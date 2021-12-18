1939d49c815c32253086702942365fcc26b0c171
arnu515 <arnu5152@gmail.com>
2021-12-18 22:17:55 +0530

    fix: Getting invites owner -> owner_id\n\nfix: return getting invites data as an object, not array

--- packages/backend/src/organisations/organisations.controller.ts
+++ packages/backend/src/organisations/organisations.controller.ts
@@ -136,10 +136,10 @@ export class OrganisationsController {
       return httpError(404, "Organisation not found");
     }
 
-    if (org.owner.toString() !== session.user.id.toString()) {
+    if (org.owner_id !== session.user.id.toString()) {
       return httpError(403, "You don't own this organisation");
     }
 
-    return await this.invitesService.getOrgInvites(id);
+    return { invites: await this.invitesService.getOrgInvites(id) };
   }
 }
