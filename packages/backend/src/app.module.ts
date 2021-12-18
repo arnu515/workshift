603decb44822a207df62f925dcd4c387ff5e0773
arnu515 <arnu5152@gmail.com>
2021-12-18 15:50:05 +0530

    feat: add prisma service for connecting to db

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AuthModule } from "./auth/auth.module";
-import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
 import { OrganisationsModule } from "./organisations/organisations.module";
+import { PrismaService } from "./prisma.service";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ cache: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
-    AuthModule,
-    OrganisationsModule
-  ],
+  imports: [ConfigModule.forRoot({ cache: true }), AuthModule, OrganisationsModule],
   controllers: [AppController],
-  providers: []
+  providers: [PrismaService]
 })
 export class AppModule {}
