f56f3985f2e463607522ca8a8ade899b22f220a9
arnu515 <arnu5152@gmail.com>
2021-12-21 12:55:57 +0530

    feat: split the spinner into a separate component

---
+++ packages/app/src/lib/components/Loader.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+</script>
+
+<div transition:fly class="spinner" />
+
+<style lang="postcss">
+	.spinner {
+		@apply border-4 border-transparent rounded-full border-dashed;
+		border-top-color: gray;
+		width: 32px;
+		height: 32px;
+		animation: spin 500ms linear infinite;
+	}
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
--- packages/app/src/lib/components/Loading.svelte
+++ packages/app/src/lib/components/Loading.svelte
@@ -1,22 +1,15 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
+	import Loader from './Loader.svelte';
 </script>
 
 <div class="fixed w-full h-full top-0 left-0 bg-gray-200 grid place-items-center">
+	<Loader />
 	<div class="flex flex-col items-center">
-		<div transition:fly class="spinner" />
 		<p class="nojs">This site requires JavaScript to work.</p>
 	</div>
 </div>
 
 <style lang="postcss">
-	.spinner {
-		@apply border-4 border-transparent rounded-full border-dashed;
-		border-top-color: gray;
-		width: 32px;
-		height: 32px;
-		animation: spin 500ms linear infinite;
-	}
 	.nojs {
 		@apply text-gray-600 text-xl text-center my-4 font-bold;
 		opacity: 0;
@@ -25,14 +18,6 @@
 		animation-timing-function: ease;
 		animation-delay: 5s;
 		animation-fill-mode: forwards;
-	}
-	@keyframes spin {
-		0% {
-			transform: rotate(0deg);
-		}
-		100% {
-			transform: rotate(360deg);
-		}
 	}
 	@keyframes nojs {
 		0% {
