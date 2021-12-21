82c6f12d523a626b4ab25a5532acc25e8e938d21
arnu515 <arnu5152@gmail.com>
2021-12-21 16:36:16 +0530

    feat: add navbar
    also format `__layout.svelte`

---
+++ packages/app/src/lib/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<nav class="bg-primary flex items-center justify-between text-white">
+	<a class="logo px-4 py-2" href="/">WorkShift</a>
+</nav>
+
+<style lang="postcss">
+	.logo {
+		@apply text-lg font-bold;
+	}
+</style>
+
