3fb7538318f40446672ec75ca11ec27c22c1be1f
arnu515 <arnu5152@gmail.com>
2021-12-23 09:12:56 +0530

    feat: add links to navbar

--- packages/app/src/lib/components/Navbar.svelte
+++ packages/app/src/lib/components/Navbar.svelte
@@ -1,10 +1,89 @@
 <nav class="bg-primary flex items-center justify-between text-white">
-	<a class="logo px-4 py-2" href="/">WorkShift</a>
+  <a class="logo px-4 py-2" href="/">WorkShift</a>
+  <div class="nav">
+    <a title="Home" aria-label="Home" href="/" class="nav-link"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg></a
+    >
+    <a
+      title="Notifications"
+      aria-label="Notifications"
+      href="/notifications"
+      class="nav-link"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+        />
+      </svg></a
+    >
+    <!-- TODO: Implement search -->
+    <button class="nav-link" title="Search" aria-label="Search"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg></button
+    >
+    <!-- TODO: Implement account button -->
+    <button class="nav-link" title="Account" aria-label="Account"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+        />
+      </svg></button
+    >
+  </div>
 </nav>
 
 <style lang="postcss">
-	.logo {
-		@apply text-lg font-bold;
-	}
-</style>
+  .logo {
+    @apply text-lg font-bold;
+  }
 
+  .nav {
+    @apply flex items-center gap-2 mr-4;
+  }
+
+  .nav .nav-link {
+    @apply p-2 rounded-full bg-transparent outline-none focus:border focus:border-white text-white hover:text-gray-200 transition-all duration-200;
+  }
+</style>
