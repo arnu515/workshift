c4f44176ed2d9f76a22f2641efe4e843e9bff2cc
arnu515 <arnu5152@gmail.com>
2021-12-23 10:38:33 +0530

    chore: format

--- packages/app/src/lib/stores/user.ts
+++ packages/app/src/lib/stores/user.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
-import type { User } from '@prisma/client';
+import { writable } from "svelte/store";
+import type { User } from "@prisma/client";
 
 export const user = writable<User | null>(null);
--- packages/app/src/lib/util/index.ts
+++ packages/app/src/lib/util/index.ts
@@ -1,10 +1,13 @@
-import { user } from '$lib/stores/user';
+import { user } from "$lib/stores/user";
 
-export function getMessage(res: { data: { message: string | string[] }; status: number }): string {
-	const { data, status } = res;
-	if (status === 401) user.set(null);
-	if (Array.isArray(data.message)) {
-		return data.message.join('\n');
-	}
-	return data.message;
+export function getMessage(res: {
+  data: { message: string | string[] };
+  status: number;
+}): string {
+  const { data, status } = res;
+  if (status === 401) user.set(null);
+  if (Array.isArray(data.message)) {
+    return data.message.join("\n");
+  }
+  return data.message;
 }
