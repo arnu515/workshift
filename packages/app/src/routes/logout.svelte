c8678ee4b5ac63defbc39401b4f73b9f7957d9e9
arnu515 <arnu5152@gmail.com>
2021-12-23 09:52:13 +0530

    feat: add logout route

---
+++ packages/app/src/routes/logout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import axios from "$lib/axios";
+  import { user } from "$lib/stores/user";
+  import { goto } from "$app/navigation";
+
+  onMount(async () => {
+    await axios.delete("/auth/logout");
+    $user = null;
+    goto("/");
+  });
+</script>
+
+<p class="text-center m-6 text-2x">Please wait, logging you out...</p>
