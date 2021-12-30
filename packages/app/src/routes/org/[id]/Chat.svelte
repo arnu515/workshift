e4309e1bd062766a6ed7c7e3c9debca99d5a4125
arnu515 <arnu5152@gmail.com>
2021-12-30 10:11:13 +0530

    feat: ability to edit and delete channels

--- packages/app/src/routes/org/[id]/Chat.svelte
+++ packages/app/src/routes/org/[id]/Chat.svelte
@@ -184,6 +184,37 @@
     }
   }
 
+  async function deleteChannel(channel: any) {
+    const res = await axios.delete(
+      `/organisations/${$organisation.id}/channels/${channel.id}`
+    );
+    if (res.status.toString().startsWith("2")) {
+      channels.refresh($organisation.id);
+      toast.push("Channel deleted", { theme: { "--toastBarBackground": "green" } });
+    } else {
+      toast.push(getMessage(res));
+    }
+  }
+
+  async function editChannel(channel: any) {
+    const newText = window.prompt("New name", channel.name);
+    console.log(newText);
+    if (!newText) {
+      toast.push("Message not edited");
+      return;
+    }
+    const res = await axios.put(
+      `/organisations/${$organisation.id}/channels/${channel.id}`,
+      { name: newText }
+    );
+    if (res.status.toString().startsWith("2")) {
+      channels.refresh($organisation.id);
+      toast.push("Channel edited", { theme: { "--toastBarBackground": "green" } });
+    } else {
+      toast.push(getMessage(res));
+    }
+  }
+
   onMount(async () => {
     channels.refresh($organisation.id);
   });
@@ -206,12 +237,54 @@
       <div class="flex flex-col">
         {#each $channels as channel}
           <button
-            on:click={() => (currentChannel = { ...channel })}
-            class="block w-full px-2 py-1 text-left hover:bg-gray-300 transition-colors duration-100 cursor-pointer {currentChannel &&
+            on:click|self={() => (currentChannel = { ...channel })}
+            class="w-full px-2 py-1 text-left hover:bg-gray-300 transition-colors duration-100 cursor-pointer flex items-center gap-2 {currentChannel &&
             currentChannel.id === channel.id
               ? 'italic bg-gray-300'
-              : ''}">{channel.name}</button
+              : ''}"
           >
+            <span>{channel.name}</span>
+            <div class="ml-auto mr-1">
+              {#if channel.owner_id === $user.id}
+                <button
+                  class="p-1 text-gray-500 hover:text-gray-600 cursor-pointer"
+                  title="Edit"
+                  aria-label="Edit"
+                  on:click={() => editChannel(channel)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                    />
+                  </svg>
+                </button>
+                <button
+                  class="p-1 text-red-500 hover:text-red-600 cursor-pointer"
+                  title="Delete"
+                  aria-label="Delete"
+                  on:click={() => deleteChannel(channel)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
+              {/if}
+            </div>
+          </button>
         {/each}
       </div>
     {/if}
