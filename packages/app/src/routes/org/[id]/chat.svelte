ee9a5212681d8c6aae017d16304ca2c05c3993b3
arnu515 <arnu5152@gmail.com>
2021-12-30 09:48:43 +0530

    feat: file messages, edit & delete

--- packages/app/src/routes/org/[id]/chat.svelte
+++ packages/app/src/routes/org/[id]/chat.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-  import {
-    organisation,
-    channels,
-    messages,
-    lastMessage
-  } from "$lib/stores/organisation";
+  import { organisation, channels, messages } from "$lib/stores/organisation";
   import user from "$lib/stores/user";
   import { onMount } from "svelte";
   import Loader from "$lib/components/Loader.svelte";
@@ -100,6 +95,95 @@
     });
   }
 
+  function uploadFile() {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.click();
+    console.log("hm");
+    input.addEventListener("change", async e => {
+      const el = e.target as HTMLInputElement;
+      if (el.files.length) {
+        const file = el.files[0];
+        // check if file is smaller than 2mb
+        if (file.size > 2097152) {
+          toast.push(
+            "Maximum file size is 2 MB. This file is " + file.size / 1024 / 1024 + " MB"
+          );
+          return;
+        }
+
+        const fd = new FormData();
+        fd.append("file", file);
+        toast.push("Uploading file", { theme: { "--toastBarBackground": "gray" } });
+        const res = await axios.post(
+          `/organisations/${$organisation.id}/channels/${currentChannel.id}/messages/file`,
+          fd
+        );
+        if (res.status.toString().startsWith("2")) {
+          toast.push("File uploaded", { theme: { "--toastBarBackground": "green" } });
+          messages.update(m => {
+            const i = currentChannel.id;
+            if (Array.isArray(m[i])) {
+              m[i].unshift(res.data);
+            } else {
+              m[i] = [res.data];
+            }
+            document
+              .getElementById(`message-${res.data?.id}`)
+              ?.scrollIntoView({ behavior: "smooth" });
+            return m;
+          });
+        } else {
+          toast.push(getMessage(res));
+        }
+      }
+    });
+  }
+
+  async function deleteMessage(message: any) {
+    const res = await axios.delete(
+      `/organisations/${$organisation.id}/channels/${currentChannel.id}/messages/${message.type}/${message.id}`
+    );
+    if (res.status.toString().startsWith("2")) {
+      messages.update(m => {
+        const i = currentChannel.id;
+        if (Array.isArray(m[i])) {
+          m[i] = m[i].filter(x => x.id !== message.id);
+        }
+        return m;
+      });
+    } else {
+      toast.push(getMessage(res));
+    }
+  }
+
+  async function editMessage(message: any) {
+    const newText = window.prompt(
+      "Edit message. Use <br> for line breaks.",
+      message.content
+    );
+    if (!newText) {
+      toast.push("Message not edited");
+      return;
+    }
+    const res = await axios.put(
+      `/organisations/${$organisation.id}/channels/${currentChannel.id}/messages/${message.type}/${message.id}`,
+      { text: newText }
+    );
+    if (res.status.toString().startsWith("2")) {
+      messages.update(m => {
+        const i = currentChannel.id;
+        if (Array.isArray(m[i])) {
+          m[i] = m[i].map(x => (x.id === message.id ? res.data : x));
+        }
+        return m;
+      });
+      toast.push("Edited message", { theme: { "--toastBarBackground": "green" } });
+    } else {
+      toast.push(getMessage(res));
+    }
+  }
+
   onMount(async () => {
     channels.refresh($organisation.id);
   });
@@ -192,8 +276,88 @@
                     class="ml-auto mr-2 text-gray-500"
                     >{dayjs(message.created_at).fromNow()}</span
                   >
+                  {#if message.user.id === $user.id}
+                    {#if message.type === "text"}
+                      <button
+                        class="p-1 text-gray-500 hover:text-gray-600 cursor-pointer"
+                        title="Edit"
+                        aria-label="Edit"
+                        on:click={() => editMessage(message)}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                          />
+                        </svg>
+                      </button>
+                    {/if}
+                    <button
+                      class="p-1 text-red-500 hover:text-red-600 cursor-pointer"
+                      title="Delete"
+                      aria-label="Delete"
+                      on:click={() => deleteMessage(message)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  {/if}
                 </div>
-                <div class="mx-1">{@html message.content}</div>
+                {#if message.type === "text"}
+                  <div class="mx-1">{@html message.content}</div>
+                {:else}
+                  <div class="mx-1 flex gap-2 items-center">
+                    <strong class="text-lg">Uploaded a file:</strong>
+                    <a
+                      href={import.meta.env.VITE_B2_BUCKET_BASE_URL.toString() +
+                        "/" +
+                        message.content}
+                      target="_blank"
+                      class="text-primary hover:underline"
+                      >{message.content.split("/").pop()}</a
+                    >
+                    <div class="ml-auto">
+                      <a
+                        href={import.meta.env.VITE_B2_BUCKET_BASE_URL.toString() +
+                          "/" +
+                          message.content}
+                        target="_blank"
+                        title="Download"
+                        aria-label="Download file"
+                        class="p-2 block bg-primary text-white hover:bg-primary-700 rounded-full cursor-pointer duration-500 transition-colors"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                          />
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                {/if}
               </div>
             {/each}
           </div>
@@ -214,6 +378,7 @@
           title="Upload"
           aria-label="Upload file"
           type="button"
+          on:click={uploadFile}
           class="p-2 bg-transparent text-primary hover:bg-sky-50 rounded-full cursor-pointer duration-500 transition-colors"
         >
           <svg
