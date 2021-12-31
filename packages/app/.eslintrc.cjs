3f5c97c76bc2579b98d64a999984eac2409da0d7
arnu515 <arnu5152@gmail.com>
2021-12-31 11:19:37 +0530

    fix: fix config

--- packages/app/.eslintrc.cjs
+++ packages/app/.eslintrc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
   plugins: ["svelte3", "@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
   overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
@@ -9,12 +13,12 @@ module.exports = {
     "svelte3/typescript": () => require("typescript")
   },
   rules: {
-    "prettier/prettier": "warn",
     "no-unused-vars": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-non-null-assertion": "off"
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": "warn"
   },
   parserOptions: {
     sourceType: "module",
